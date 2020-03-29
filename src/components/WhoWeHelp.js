1f8ce58c791202c7516f52645de0898d89086446
Michał Rogalski <michal.rogalski@simeo.io>
2020-03-29 22:06:07 +0200

    whowehelp component and contact form styles changes

--- src/components/WhoWeHelp.js
+++ src/components/WhoWeHelp.js
@@ -6,18 +6,22 @@ class Pagination extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            names: this.props.data,
+            names: this.props.organizations,
             currentPage: 1,
             namesPerPage: 3,
             // border: ""
         }
     }
+
+
     handleClick = (event, i) => {
         this.setState({
             currentPage: i,
             // border: "0.75px solid #3c3c3c"
         })
-    }
+    };
+
+
     render() {
         const { names, currentPage, namesPerPage } = this.state;
 
@@ -28,12 +32,19 @@ class Pagination extends Component {
         const elements = currentNames.map((org, i) => {
             return (
                 <>
-                    <li key={i}> {org.name} </li>
-                    <li key={i}> {org.description}</li>
-                    <li key={i}> {org.tags}</li>
+                <div className="organizations-component_inside">
+                    <div className="organizations-names">
+                        <h1 key={i}> {org.name} </h1>
+                        <span key={i}> {org.description}</span>
+                    </div>
+                    <div className="organizations-tags">
+                        <h5 key={i}> {org.tags}</h5>
+                    </div>
+                </div>
                 </>
             )
         });
+
 
         const pageNumbers = [];
         for (let i = 1; i <= Math.ceil(names.length / namesPerPage); i++) {
@@ -46,10 +57,13 @@ class Pagination extends Component {
             pageNumbers.push(element)
         }
 
+
         return <section>
-            <ul className="names">
-                {elements}
-            </ul>
+            <div className="organizations-component">
+                <ul className="names">
+                    {elements}
+                </ul>
+            </div>
             <ul className="pagination">
                 {pageNumbers}
             </ul>
@@ -60,60 +74,112 @@ class Pagination extends Component {
 class WhoWeHelp extends Component {
 
     state = {
-        userSelection: "Organizations"
+        userSelection: [ "Foundations", "Organizations"]
     }
 
     render() {
-        const Organizations = {
-            description: "W naszej bazie znajdziesz listę zweryfikowanych Organizacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+
+        const Foundations = {
+            description: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
             data: [
                 {
-                    name: "Nonprofit Organization 1",
+                    name: "Nonprofit Foundation 1",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                     tags: ["lorem ipsum"]
                 },
                 {
-                    name: "Nonprofit Organization 2",
+                    name: "Nonprofit Foundation 2",
                     description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                     tags: ["dolor sit amet"]
                 },
                 {
-                    name: "Nonprofit Organization 3",
+                    name: "Nonprofit Foundation 3",
                     description: "Sed posuere urna nec tincidunt praesent semper feugiat nibh.",
                     tags: ["lacus sed viverra tellus"]
                 },
                 {
-                    name: "Nonprofit Organization 4",
+                    name: "Nonprofit Foundation 4",
                     description: "Lectus proin nibh nisl condimentum id venenatis a condimentum.",
                     tags: ["dolor sit amet"]
                 },
                 {
-                    name: "Nonprofit Organization 5",
+                    name: "Nonprofit Foundation 5",
                     description: "Diam donec adipiscing tristique risus nec feugiat in fermentum.",
                     tags: ["lacus sed viverra tellus"]
                 },
                 {
-                    name: "Nonprofit Organization 6",
+                    name: "Nonprofit Foundation 6",
                     description: "Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus.",
                     tags: ["lorem ipsum"]
                 },
                 {
-                    name: "Nonprofit Organization 7",
+                    name: "Nonprofit Foundation 7",
                     description: "Sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit.",
                     tags: ["dolor sit amet"]
                 },
                 {
-                    name: "Nonprofit Organization 8",
+                    name: "Nonprofit Foundation 8",
                     description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                     tags: ["volutpat commodo sed"]
                 },
                 {
-                    name: "Nonprofit Organization 9",
+                    name: "Nonprofit Foundation 9",
                     description: "Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc.",
                     tags: ["donec adipiscing tristique"]
                 }
             ]
         }
+        const Organizations = {
+            description: "W naszej bazie znajdziesz listę zweryfikowanych Organizacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+            data: [
+                {
+                    name: `Nonprofit “Organization 1”`,
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    tags: ["lorem ipsum"]
+                },
+                {
+                    name: `Nonprofit “Organization 2”`,
+                    description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    tags: ["dolor sit amet"]
+                },
+                {
+                    name: `Nonprofit “Organization 3”`,
+                    description: "Sed posuere urna nec tincidunt praesent semper feugiat nibh.",
+                    tags: ["lacus sed viverra tellus"]
+                },
+                {
+                    name: `Nonprofit “Organization 4”`,
+                    description: "Lectus proin nibh nisl condimentum id venenatis a condimentum.",
+                    tags: ["dolor sit amet"]
+                },
+                {
+                    name: `Nonprofit “Organization 5”`,
+                    description: "Diam donec adipiscing tristique risus nec feugiat in fermentum.",
+                    tags: ["lacus sed viverra tellus"]
+                },
+                {
+                    name: `Nonprofit “Organization 6”`,
+                    description: "Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus.",
+                    tags: ["lorem ipsum"]
+                },
+                {
+                    name: `Nonprofit “Organization 7”`,
+                    description: "Sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit.",
+                    tags: ["dolor sit amet"]
+                },
+                {
+                    name: `Nonprofit “Organization 8”`,
+                    description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    tags: ["volutpat commodo sed"]
+                },
+                {
+                    name: `Nonprofit “Organization 9”`,
+                    description: "Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc.",
+                    tags: ["donec adipiscing tristique"]
+                }
+            ]
+        }
+
 
         return (
             <div className="who-we_help_component">
@@ -122,7 +188,7 @@ class WhoWeHelp extends Component {
                     <img src={Decoration} alt="Decoration" />
                 </div>
                 <div className="choice-buttons">
-                    <p>Fundacjom</p>
+                    <p onClick="">Fundacjom</p>
                     <p>Organizacjom <br />pozarządowym</p>
                     <p>Lokalnym <br />zbiórkom</p>
                 </div>
@@ -131,7 +197,10 @@ class WhoWeHelp extends Component {
                         <p>{Organizations.description}</p>
                     </div>
                 </div>
-                <Pagination data={Organizations.data}/>
+                <Pagination 
+                    foundations={Foundations.data}
+                    organizations={Organizations.data}
+                />
             </div>
         );
     }
