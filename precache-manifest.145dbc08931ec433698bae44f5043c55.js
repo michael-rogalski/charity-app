110102a7c79c8aeb79833533fbfb02044b1eac47
Michał Rogalski <michal.rogalski@simeo.io>
2020-04-18 18:13:22 +0200

    Updates

---
+++ precache-manifest.145dbc08931ec433698bae44f5043c55.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e29df22b00d614d4f6340f43102052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b363732828bbbaa507dc",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "83e046cd712454d0f733",
+    "url": "/static/css/main.0dbe1519.chunk.css"
+  },
+  {
+    "revision": "b363732828bbbaa507dc",
+    "url": "/static/js/2.56fe1394.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.56fe1394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e046cd712454d0f733",
+    "url": "/static/js/main.6423dfb8.chunk.js"
+  },
+  {
+    "revision": "bed978285d35c1376a71",
+    "url": "/static/js/runtime-main.382ac891.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/static/media/3_Columns_Background.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/static/media/Signature.e54cfcfc.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/charity-app/precache-manifest.9b5b7abeed10bee31ba96169c1e9a420.js"
+  "/precache-manifest.145dbc08931ec433698bae44f5043c55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/charity-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
